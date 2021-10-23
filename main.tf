9e41c17471bf05621e1e7929ba97f26daac16325
LBGarber <lgarber@linode.com>
2021-10-23 13:32:51 -0400

    Add vars

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
 resource "linode_instance" "parent" {
-  label = "swarm-parent"
+  label = "${var.label_prefix}-swarm-parent"
   image = "linode/ubuntu20.04"
-  region = "us-southeast"
-  type = "g6-standard-1"
+  region = var.region
+  type = var.parent_type
   authorized_keys = [chomp(file(var.ssh_public_key))]
 
   connection {
@@ -14,6 +14,29 @@ resource "linode_instance" "parent" {
 
   provisioner "remote-exec" {
     script = "${path.module}/scripts/provision_parent.sh"
+  }
+}
+
+resource "linode_instance" "child" {
+  count = var.child_count
+
+  depends_on = [local_file.rendered_provisioner]
+
+  label = "${var.label_prefix}-swarm-child-${count.index}"
+  image = "linode/ubuntu20.04"
+  region = var.region
+  type = var.child_type
+  authorized_keys = [chomp(file(var.ssh_public_key))]
+
+  connection {
+    type = "ssh"
+    user = "root"
+    private_key = chomp(file(var.ssh_private_key))
+    host = self.ip_address
+  }
+
+  provisioner "remote-exec" {
+    script = "${path.module}/tmp/provisioner-${count.index}.sh"
   }
 }
 
@@ -36,27 +59,4 @@ resource "local_file" "rendered_provisioner" {
 
   content  = templatefile("${path.module}/scripts/provision_child.sh.tpl", {token=data.external.grab_token.result.token, public_ip=linode_instance.parent.ip_address})
   filename = "${path.module}/tmp/provisioner-${count.index}.sh"
-}
-
-resource "linode_instance" "child" {
-  count = var.child_count
-
-  depends_on = [local_file.rendered_provisioner]
-
-  label = "swarm-child-${count.index}"
-  image = "linode/ubuntu20.04"
-  region = "us-southeast"
-  type = "g6-standard-1"
-  authorized_keys = [chomp(file(var.ssh_public_key))]
-
-  connection {
-    type = "ssh"
-    user = "root"
-    private_key = chomp(file(var.ssh_private_key))
-    host = self.ip_address
-  }
-
-  provisioner "remote-exec" {
-    script = "${path.module}/tmp/provisioner-${count.index}.sh"
-  }
 }
--- variables.tf
+++ variables.tf
@@ -2,6 +2,22 @@ variable "linode_token" {
   sensitive = true
 }
 
+variable "parent_type" {
+  default = "g6-standard-2"
+}
+
+variable "child_type" {
+  default = "g6-standard-2"
+}
+
+variable "region" {
+  default = "us-southeast"
+}
+
+variable "label_prefix" {
+  default = "tf"
+}
+
 variable "child_count" {
   default = 6
 }
